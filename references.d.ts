ed89aa8d9a8179084a725e26e9d63be78562bd82
Robel <robalempok@gmail.com>
2018-04-09 04:24:06 -0700

    update framework

--- references.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="./src/node_modules/tns-platform-declarations/ios.d.ts" />
-/// <reference path="./src/node_modules/tns-platform-declarations/android.d.ts" />
-/// <reference path="./objc!ALVideoAudioCall.d.ts" />
-/// <reference path="./objc!Applozic.d.ts" />
-/// <reference path="./objc!TwilioVideo.d.ts" />
---
+++ src/platforms/ios/ALVideoAudioCall.framework/Headers/ALVideoAudioCall.h
@@ -0,0 +1,20 @@
+//
+//  ALVideoAudioCall.h
+//  ALVideoAudioCall
+//
+//  Created by PSIHPOK on 4/6/18.
+//  Copyright © 2018 PSIHPOK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ALVideoAudioCall.
+FOUNDATION_EXPORT double ALVideoAudioCallVersionNumber;
+
+//! Project version string for ALVideoAudioCall.
+FOUNDATION_EXPORT const unsigned char ALVideoAudioCallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALVideoAudioCall/PublicHeader.h>
+
+#import "ALAudioVideoCallVC.h"
+#import <ALVideoAudioCall/ALAudioVideoCallVC.h>
